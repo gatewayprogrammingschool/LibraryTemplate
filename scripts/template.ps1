7a56a119b78de15dbfe90bce3bba4b88e5d071e1
The Sharp Ninja <ninja@thesharp.ninja>
2021-12-22 02:07:28 -0600

    Template script

---
+++ scripts/template.ps1
@@ -0,0 +1,78 @@
+function Get-TemplateProperties {
+    pushd
+    $ScriptDirName = Split-Path $script:MyInvocation.MyCommand.Path
+    if($ScriptDirName -ine "scripts") {
+        $scriptsDir = Get-ChildItem scripts -Path $PWD -ErrorAction Stop
+
+        if(-not $scriptsDir) {
+            throw "Cannot locate scripts directory."
+        }
+    } else {
+        $scriptsDir = Get-Item $PWD
+    }
+
+    $scriptsParentDir = $scriptsDir.PSParentPath
+
+    Set-Location $scriptsParentDir
+
+    $propsFile = Get-ChildItem Directory.Build.props -ErrorAction Stop
+
+    if(-not $propsFile) {
+        throw "Cannot locate Directory.Build.props in $scriptsParentDir"
+    }
+
+    [xml]$props=Get-Content $propsFile
+
+    $group = $props.Project.PropertyGroup
+
+    $properties = @{}
+    $group.ChildNodes | ForEach-Object -Process {
+        $element = $_
+        $properties.Add($element.Name, $element.InnerText)
+    }
+
+    $properties
+}
+
+function Set-TemplateValues {
+    $properties = Get-TemplateProperties
+
+    if($properties) {
+        $files = Get-ChildItem *.cs,*.sln,*.md,*.yml,*.json -Recurse
+
+        if($files) {
+            $files | ForEach-Object -Process {
+                $file = $_
+
+                $contents = $file | Get-Content
+
+                $fileChanged = $false;
+
+                "Searching in $file [${contents.Length} lines]"
+
+                for($index=0; $index -lt $contents.Length; $index += 1) {
+                    $line = $contents[$index]
+
+                    foreach($property in $properties) {
+                        while($line -match $property.Name) {
+                            $original = $line
+                            $line = $line.Replace($property.Name, $property.Value)
+                            $fileChanged = $true
+                            "Replaced ${property.Name} with ${property.Value} in $original"
+                        }
+                    }
+
+                    $contents[$index] = $line
+                }
+
+                if($fileChanged) {
+                    $contents | Out-File $file
+
+                    "Updated $file"
+                }
+            }
+        }
+    }
+}
+
+Set-TemplateValues
